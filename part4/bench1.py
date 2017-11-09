416cc5dd67094d5decf09188b55c50fa30e0e56a
Maciej Fijalkowski <fijall@gmail.com>
2017-11-09 07:01:59 -0800

    more benchmarks

--- part4/bench1.py
+++ part4/bench1.py
@@ -1,13 +1,35 @@
 
 from proto.serializer import serialize, unserialize
 
+def bench_serialize():
+    d = {}
+    l = [None]
+    for i in range(100):
+        d[str(i) + "foo"] = "foobarbaz" * 20
+    for i in range(100000):
+        l[0] = serialize(d)
+
 def bench_unserialize():
     d = {}
     for i in range(100):
-        d[str(i) + "foo"] = [1, 2, 3]
+        d[str(i) + "foo"] = "foobarbaz" * 20
     s = serialize(d)
     for i in range(10000):
         unserialize(s)
 
-bench_unserialize()
+def bench_access():
+    d = {}
+    l = [None, None, None]
+    for i in range(100):
+        d[str(i) + "foo"] = "foobarbaz" * 20
+    s = serialize(d)
+    u = unserialize(s)
+    for i in range(10000000):
+        l[0] = u["13foo"]
+        l[1] = u["14foo"]
+        l[2] = u["15foo"]
 
+
+#bench_unserialize()
+bench_access()
+#bench_serialize()
