180be501faefa449db9111dce6613a8d63e8784c
Maciej Fijalkowski <fijall@gmail.com>
2017-11-09 07:03:50 -0800

    update

--- part4/bench1.py
+++ part4/bench1.py
@@ -11,11 +11,12 @@ def bench_serialize():
 
 def bench_unserialize():
     d = {}
+    l = [None]
     for i in range(100):
         d[str(i) + "foo"] = "foobarbaz" * 20
     s = serialize(d)
-    for i in range(10000):
-        unserialize(s)
+    for i in range(100000):
+        l[0] = unserialize(s)
 
 def bench_access():
     d = {}
@@ -30,6 +31,6 @@ def bench_access():
         l[2] = u["15foo"]
 
 
-#bench_unserialize()
-bench_access()
+bench_unserialize()
+#bench_access()
 #bench_serialize()
